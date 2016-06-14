fd91e3342cabd375dc44c7a05cd407d85d161381
Johnsoniscoming <tengors888@yahoo.com.tw>
2016-06-14 14:37:48 +0800

    2016/6/13
    class

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/Drink.java
@@ -0,0 +1,27 @@
+package com.example.user.simpleui;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ * Created by J.H on 2016/6/13.
+ */
+public class Drink {
+    String name;
+    int mPrice;
+    int lPrice;
+    int imageId;
+
+    public JSONObject getData()
+    {
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            jsonObject.put("name", name);
+            jsonObject.put("price", mPrice);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+}
--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.user.simpleui;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +17,7 @@ import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,6 +25,8 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_CODE_DRINK_MENU_ACTIVITY = 0;
 
     TextView textView;
     EditText editText;
@@ -36,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
@@ -57,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        Log.d("Debug", "Main Activity OnCreate");
     }
 
     void setupListView()
@@ -88,5 +98,60 @@ public class MainActivity extends AppCompatActivity {
         setupListView();
     }
 
+    public void goToMenu(View view)
+    {
+        Intent intent = new Intent();
+        intent.setClass(this, DrinkMenuActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_DRINK_MENU_ACTIVITY);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE_DRINK_MENU_ACTIVITY)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                textView.setText(data.getStringExtra("results"));
+                Toast.makeText(this, "完成菜單", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Debug", "Main Activity OnStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Debug", "Main Activity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Debug", "Main Activity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Debug", "Main Activity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Debug", "Main Activity onDestroy");
+    }
+
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("Debug", "Main Activity onRestart");
+    }
 }
